b602a831613071e860c2ec84b50a60d02eeb9d54
Kirill Rozov <kirich1409@gmail.com>
2017-07-29 15:11:18 +0300

    Update java docs Kotlin StrictModeCompat extensions

--- strict-mode-compat-kotlin/src/main/java/com/kirillr/strictmodehelper/kotlin/StrictModeCompat.kt
+++ strict-mode-compat-kotlin/src/main/java/com/kirillr/strictmodehelper/kotlin/StrictModeCompat.kt
@@ -1,11 +1,27 @@
 package com.kirillr.strictmodehelper.kotlin
 
+import com.kirillr.strictmodehelper.StrictModeCompat
+
 /**
+ * Kotlin extensions for {@link com.kirillr.strictmodehelper.StrictModeCompat}
+ *
  * @authror Kirill Rozov
  * @date 29/7/17.
  */
 object StrictModeCompat {
 
+    /**
+     * For code to note that it's slow.  This is a no-op unless the
+     * current thread's {@link android.os.StrictMode.ThreadPolicy} has
+     * {@link android.os.StrictMode.ThreadPolicy.Builder#detectCustomSlowCalls}
+     * enabled.
+     *
+     * @param lazyMessage Short string for the exception stack trace that's
+     *                    built if when this fires.
+     *
+     * @see StrictModeCompat#noteSlowCall(String, Object...)
+     * @see StrictModeCompat#noteSlowCall(Locale, String, Object...)
+     */
     @JvmStatic
     fun noteSlowCall(lazyMessage: () -> String) {
         com.kirillr.strictmodehelper.StrictModeCompat.noteSlowCall(lazyMessage())
