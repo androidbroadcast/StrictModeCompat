95cab7214b12198aaab7f74ed2f7606a91fb182d
Kirill Rozov <kirich1409@gmail.com>
2018-06-13 21:23:29 +0300

    Add Travis CI integration

---
+++ scripts/publish.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+./gradlew :strict-mode-compat:assembleRelease \
+    :strict-mode-compat-kotlin:assembleRelease \
+    :strict-mode-compat:bintrayUpload \
+    :strict-mode-compat-kotlin:bintrayUpload \
+    -PdisablePreDex
