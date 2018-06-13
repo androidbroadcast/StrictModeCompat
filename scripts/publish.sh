4b34ed0976799b3fd7a7868014e6a344c9085e38
Kirill Rozov <kirich1409@gmail.com>
2018-06-13 21:58:48 +0300

    Update script execution rights

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-./gradlew :strict-mode-compat:assembleRelease \
-    :strict-mode-compat-kotlin:assembleRelease \
-    :strict-mode-compat:bintrayUpload \
-    :strict-mode-compat-kotlin:bintrayUpload \
-    -PdisablePreDex
+./gradlew :strict-mode-compat:assembleRelease :strict-mode-compat-kotlin:assembleRelease :strict-mode-compat:bintrayUpload :strict-mode-compat-kotlin:bintrayUpload -PdisablePreDex
